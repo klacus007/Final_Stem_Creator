f206a1239b68b64273538a0c95c3f10ea5198427
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-10-06 20:18:35 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -11,3 +11,4 @@ pip3 install tensorflow-gpu==1.15
 unzip "/content/Final_Stem_Creator/gpac.zip"
 chmod -R 755 "/content/Final_Stem_Creator/"
 chmod -R 755 "/content/Final_Stem_Creator/gpac/"
+rm -rf "/content/Final_Stem_Creator/__MACOSX/"
