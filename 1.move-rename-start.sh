84bcf2765e10aa4270aa10690a263cae6de7deb9
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-10-06 20:13:12 -0300

    Update 1.move-rename-start.sh

--- 1.move-rename-start.sh
+++ 1.move-rename-start.sh
@@ -1,10 +1,10 @@
 cd "/content/drive/My Drive/MyStems/originals"
 
-for f in *_PN.aiff; do
-    mv -- "$f" "${f%_PN.aiff}_PN.5.master.aiff"
+for f in *.aiff; do
+    mv -- "$f" "${f%.aiff}.5.master.aiff"
 done
 
-for f in *_PN.5.master.aiff; do
+for f in *.5.master.aiff; do
     mv "$f" "/content/drive/My Drive/MyStems/spleeter"
 done
 
