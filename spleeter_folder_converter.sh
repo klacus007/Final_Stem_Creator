aeebb4e51ef2243ca24a764700ea78b3053bdfc4
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-10-06 19:31:50 -0300

    Update spleeter_folder_converter.sh

--- spleeter_folder_converter.sh
+++ spleeter_folder_converter.sh
@@ -1,5 +1,5 @@
 cd "/content/drive/My Drive/MyStems/originals"
 
-bash "./fix_name.sh"
+bash "/content/Final_Stem_Creator/fix_name.sh"
 
-for f in *.*; do spleeter separate -B tensorflow -c m4a -p "./base_config.json" -f "{filename}.{instrument}.{codec}" -i "$f" -o "/content/drive/My Drive/MyStems/spleeter" ; done 
+for f in *.*; do spleeter separate -B tensorflow -c m4a -p "/content/Final_Stem_Creator/base_config.json" -f "{filename}.{instrument}.{codec}" -i "$f" -o "/content/drive/My Drive/MyStems/spleeter" ; done 
