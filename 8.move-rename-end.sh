2a77430fea9666d50e04798142d0be9e49edbf1c
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-10-06 20:14:47 -0300

    Update 8.move-rename-end.sh

--- 8.move-rename-end.sh
+++ 8.move-rename-end.sh
@@ -10,7 +10,7 @@ mv "$f" "/content/drive/My Drive/MyStems/stems/";done
 cd "/content/drive/My Drive/MyStems/stems/"
 
 for f in *.5.master.stem.m4a; do
-    mv -- "$f" "${f%_PN.5.master.stem.m4a}.stem.m4a"
+    mv -- "$f" "${f%.5.master.stem.m4a}.stem.m4a"
 done
 
 cd '/content/drive/My Drive/MyStems/spleeter/'
@@ -23,8 +23,8 @@ for f in *.6.tags.json; do
     rm "$f"
 done
 
-for f in *_PN.5.master.aiff; do
-    mv -- "$f" "${f%_PN.5.master.aiff}_PN.aiff"
+for f in *.5.master.aiff; do
+    mv -- "$f" "${f%.5.master.aiff}.aiff"
 done
 
 for f in *.aiff; do
